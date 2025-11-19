8e5b9741f67973df7ba0c75453b1768919fe6388
manishchauhan83 <manishchauhan8355@gmail.com>
2025-11-19 23:02:00 +0530

    api new patient update

--- src/routes/patient.route.js
+++ src/routes/patient.route.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { createPatientController ,getAllPatientController} = require("../controller/patient.controller");
+const {
+  createPatientController,
+  getAllPatientController,
+  updatePatientControll,
+} = require("../controller/patient.controller");
 
-router.post("/create-patien",createPatientController)
-router.get("/get-patient",getAllPatientController)
-module.exports=router;
+router.post("/create-patien", createPatientController);
+router.get("/get-patient", getAllPatientController);
+router.put("/update-patient/:patientId", updatePatientControll);
+module.exports = router;
